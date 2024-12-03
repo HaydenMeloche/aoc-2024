de3bcd61b5acff0f1d12ea84ff267282f51ccf0d
Hayden <meloche.hayden@gmail.com>
2024-12-02 22:01:01 -0500

    completed day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,36 @@
+import kotlin.collections.mutableMapOf
+import kotlin.collections.unzip
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = input
+            .map { line ->
+                val (left, right) = line.split(Regex("\\s+")).map { it.toInt() }
+                left to right
+            }.unzip()
+
+        val sortedLeft = left.sorted()
+        val sortedRight = right.sorted()
+
+        return sortedLeft.zip(sortedRight).sumOf { (left, right) ->
+            abs(right - left)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val leftList = mutableListOf<Int>()
+        val rightMap = mutableMapOf<Int, Int>()
+        input.forEach {
+            val (left, right) = it.split(Regex("\\s+")).map { it.toInt() }
+            leftList.add(left)
+            rightMap[right] = rightMap.getOrDefault(right, 0) + 1
+        }
+
+        return leftList.sumOf { it * (rightMap[it] ?: 0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
